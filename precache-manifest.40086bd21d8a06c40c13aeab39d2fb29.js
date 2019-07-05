2f746c01a9c7c25d43c0d799113eb2c11436bc00
vatergo <vatergo@mail.ru>
2019-07-05 22:41:31 +0500

    Updates

---
+++ precache-manifest.40086bd21d8a06c40c13aeab39d2fb29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2d44534a3458b553e7b",
+    "url": "/morphis/static/js/runtime~main.8882f460.js"
+  },
+  {
+    "revision": "895b7abd8b6d149df546",
+    "url": "/morphis/static/js/main.d6beec40.chunk.js"
+  },
+  {
+    "revision": "c786969582857d6e48b8",
+    "url": "/morphis/static/js/2.351354d8.chunk.js"
+  },
+  {
+    "revision": "c786969582857d6e48b8",
+    "url": "/morphis/static/css/2.8c645362.chunk.css"
+  },
+  {
+    "revision": "519f24ce5d5295f6b882472fce5a2f23",
+    "url": "/morphis/index.html"
+  }
+];
